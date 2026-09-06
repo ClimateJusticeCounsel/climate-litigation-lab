d542cd9c9160eeb7362b480d4ee425fa4b783c3f
ClimateJusticeCounsel <climatejusticecounsel@gmail.com>
2026-09-06 00:07:27 -0400

    Add embedded Sanity Studio config

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+'use client'
+
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { dataset, projectId } from './sanity/env'
+import { schemaTypes } from './sanity/schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Climate Litigation Lab',
+  basePath: '/studio',
+  projectId,
+  dataset,
+  plugins: [structureTool()],
+  schema: {
+    types: schemaTypes,
+  },
+})

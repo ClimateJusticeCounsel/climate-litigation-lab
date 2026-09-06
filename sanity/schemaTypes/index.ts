58a60618dae01395f7d52e5b7536d6b5a51f5e75
ClimateJusticeCounsel <climatejusticecounsel@gmail.com>
2026-09-06 00:07:21 -0400

    Register Sanity schemas

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,9 @@
+import { eventType, programItemType, resourceLinkType } from './event'
+import { speakerType } from './speaker'
+
+export const schemaTypes = [
+  eventType,
+  speakerType,
+  programItemType,
+  resourceLinkType,
+]

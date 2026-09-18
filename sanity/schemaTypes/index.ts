3170d9eb9b6d6288c879a90130d4ac1561443523
ClimateJusticeCounsel <climatejusticecounsel@gmail.com>
2026-09-17 23:37:45 -0400

    Register News and Analysis content type

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,10 @@
 import { eventType, programItemType, resourceLinkType } from './event'
+import { newsArticleType } from './newsArticle'
 import { speakerType } from './speaker'
 
 export const schemaTypes = [
   eventType,
+  newsArticleType,
   speakerType,
   programItemType,
   resourceLinkType,
